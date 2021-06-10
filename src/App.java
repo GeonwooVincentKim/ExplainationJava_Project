6f780d8542a10eaa17d9212ea335e8fe949993a2
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-10 09:34:56 +0900

    [Feature] - Successfully implemented For-loop and if for isContains Method

--- src/App.java
+++ src/App.java
@@ -2,8 +2,6 @@ import static java.lang.System.out;
 
 import java.util.ArrayList;
 import java.util.List;
-import Feature.ArrayList.ArrayListTest;
-import Feature.ArrayList.CustomArrayList;
 
 public class App {
     public static int arrayFrequency(List<Integer> randomList, int number) {
@@ -17,22 +15,17 @@ public class App {
         return count;
     }
 
-    public boolean isContains(Object o, int[] getRandomArray) {
-        int[] elementData = getRandomList(getRandomArray);
-        if (o == null) {
-            for (int i = 0; i < elementData.length; i++) {
-                if (o == null) {
-                    return true;
-                }
-            }
-        } else {
-            for (int i = 0; i < elementData.length; i++) {
-                if (o.equals(elementData[i])) {
-                    return true;
-                }
+    public static boolean isContains(List<Integer> countArray, int temp) {
+        boolean isContainValue = false;
+
+        for (int i = 0; i < countArray.size(); i++) {
+            if (countArray.get(i) == temp) {
+                isContainValue = true;
+                return isContainValue;
             }
         }
-        return false;
+
+        return isContainValue;
     }
 
     public boolean contains(CharSequence sequence) {
@@ -103,17 +96,10 @@ public class App {
 
         // int temp = 0;
         int[] temp = new int[randomValue]; // 랜덤 길이 만큼 숫자 범위 11 ~ 99의 랜덤 수(난수)들을 넘겨 받는다.
-        List<Integer> randomList = new ArrayList<Integer>();
-        List<Integer> countList = new ArrayList<Integer>();
-        // List<Integer> countList = new CustomArrayList<Integer>();
-        List<Integer> testList = new CustomArrayList<Integer>();
-        ArrayListTest arTest = new ArrayListTest();
-
-        // int[] randomArray = new int[100]; // temp 배열의 값을 저장한다.
-        // int[] countArray = new int[100]; // 중복을 제거한 값들을 countArray 에 저장한다.
+        List<Integer> randomList = new ArrayList<Integer>(); // temp 배열의 값을 저장한다.
+        List<Integer> countList = new ArrayList<Integer>(); // 중복을 제거한 값들을 countArray 에 저장한다.
 
         for (int i = 0; i < randomValue; i++) {
-            // temp = (int) (Math.random() * (99 - 11 + 1) + 11);
             temp[i] = (int) (Math.random() * (99 - 11 + 1)) + 11;
             out.print(temp[i] + " ");
         }
@@ -132,112 +118,15 @@ public class App {
         // ArrayList 로 여러 번 나오는 수의 빈도 값 탐색 및 겹치는 수 확인
         // i 와 j 두 개를 만들어놓고, 각각 randomList 라는 ArrayList 에서 겹치는 수 확인 후,
         // count 값에 저장한다.
-        int mode = 0;
-        int count = 0;
-
-        // for (int i = 0; i < randomValue; i++) {
         for (int i = 0; i < randomList.size(); i++) {
-            // for (int j = i + 1; j < randomList.size(); j++) {
-            // if (countList.get(j) == countList.get(j)) {
-            // // countList.get(j) = null;
-            // // count = -1;
-            // count = countList.get(j);
-            // count = -1;
-            // }
-            // }
-
-            // for (int j = 0; j < randomList.size(); j++) {
-            // if (i == j) {
-            // // countList.add(temp[i]);
-            // } else if (randomList.get(j).equals(randomList.get(i))) {
-            // randomList.remove(j);
-            // countList.add(randomList.get(i));
-            // }
-            // }
-
-            // for (int j = i + 1; j < randomValue; j++) {
-            // if (countList.get(j) == randomList.get(i)) {
+            // Original Code
+            // if (!countList.contains(temp[i])) {
             // countList.add(temp[i]);
             // }
-            // }
-            // if (countList.equals(temp)) {
-
-            // }
-
-            // Original Code
-            if (!countList.contains(temp[i])) {
+            if (!(isContains(countList, temp[i]))) {
                 countList.add(temp[i]);
             }
-
-            // if (count >= 0) {
-            // for (int j = 0; j < randomList.size(); j++) {
-            // if (randomList.get(i) == randomList.get(j)) {
-            // count++;
-            // // if (randomList.get(i) > randomList.get(j)) {
-            // // // mode = count;
-            // countList.add(temp[j]);
-
-            // // if (count > 0) {
-            // // countList.remove(temp[j]);
-            // // }
-
-            // // }
-            // // countList.add(randomList.get(j));
-            // // randomList.remove(j);
-            // }
-            // // countList.add(temp[i]);
-            // }
-            // } else {
-
-            // }
-            // if (count > 0) {
-            // // countList.add(temp[i]);
-            // countList.remove(temp[i]);
-            // i--;
-            // }
-            // if (count > 1) {
-            // countList.add(temp[i]);
-            // for (int k = 0; k < randomList.size(); k++) {
-            // if (randomList.get(i) == randomList.get(k) && i != k) {
-            // randomList.remove(k);
-            // k--;
-            // }
-            // }
-            // }
-            // if (randomList.get(i) > 0) {
-            // countList.remove(i);
-            // }
-
-            // for (int j = 0; j < temp.length; j++) {
-            // if (randomList.get(i) > 0) {
-            // countList.add(temp[j]);
-            // }
-            // }
-
-            // // 굳이 필요할까..??
-            // if (count >= mode && count > 1) {
-            // mode = count;
-            // // countList.add(randomList.get(i));
-            // // countList.add(mode);
-
-            // // for (int k = 0; k < randomList.size(); k++) {
-            // // if (randomList.get(i) == randomList.get(k) && i != k) {
-            // // randomList.remove(k);
-            // // k--;
-            // // }
-            // // }
-            // }
-
-            // // out.print(countList + " ");
-
-            // out.print(count + " ");
         }
-
-        // for (int i = 0; i < randomList.size(); i++) {
-        // if (countList.get(i) != -1) {
-
-        // }
-        // }
 
         out.println();
 
@@ -254,81 +143,30 @@ public class App {
                 // out.print(temp[j] + " ");
                 currentValue = countList.get(i);
                 nextValue = countList.get(j);
-                // out.print(currentValue + " ");
-                // out.print(nextValue + " ");
 
                 currentCount = arrayFrequency(randomList, currentValue);
                 nextCount = arrayFrequency(randomList, nextValue);
 
-                int tempValue = 0;
                 if (currentCount < nextCount) {
-                    tempValue = currentValue;
-                    countList.set(i, nextValue);
-                    countList.set(j, tempValue);
+                    bubbleSort(countList, currentValue, nextValue, i, j);
                 } else if (currentCount == nextCount && currentValue < nextValue) {
-                    tempValue = currentValue;
-                    countList.set(i, nextValue);
-                    countList.set(j, tempValue);
+                    bubbleSort(countList, currentValue, nextValue, i, j);
                 }
             }
-            // out.println();
         }
 
         // randomList 출력
-        // for (int i = 0; i < randomList.size(); i++) {
-        // out.print(randomList.get(i) + " ");
-        // }
-
         out.println();
 
         // countList 출력
-        // if (countList.size() >= 1) {
-        // int Bindo = 0;
-
-        // for (int i = 1; i < countList.size(); i = i + 2) {
-        // if (countList.get(i) > Bindo) {
-        // Bindo = countList.get(i);
-        // }
-        // // out.print(Bindo + " ");
-        // }
-        // }
         result = printArray(randomList, countList, printValue);
-
         out.println(result);
+    }
 
-        // for (int i = 0; i < countList.size(); i++) {
-        // out.print("#" + (i + 1) + " " + countList.get(i));
-        // out.println("(" + arrayFrequency(randomList, countList.get(i)) + ")");
-        // }
-
-        // integer 배열로 여러 번 나오는 수의 빈도 값을 탐색
-        // for (int i = 0; i < randomValue; i++) {
-        // randomArray[i] = temp;
-        // if (countArray[temp] == 0) {
-        // countValue++;
-        // // out.print(countValue + " " + temp);
-        // }
-
-        // out.println();
-        // countArray[temp]++;
-        // // out.print(countArray[i] + " ");
-        // }
-
-        // int idx = 0;
-        // int[] getrandomArray = new int[countValue];
-        // int[] getcountArray = new int[countValue];
-
-        // for (int num = 11; num <= 99; num++) {
-        // if (countArray[num] > 0) {
-        // getrandomArray[idx] = countArray[idx];
-        // getcountArray[idx] = num;
-        // out.print(getrandomArray[idx] + " ");
-        // out.print(getcountArray[idx] + " ");
-        // idx++;
-
-        // }
-        // // out.print(idx + " ");
-        // }
-
+    private static void bubbleSort(List<Integer> countList, int currentValue, int nextValue, int i, int j) {
+        int tempValue;
+        tempValue = currentValue;
+        countList.set(i, nextValue);
+        countList.set(j, tempValue);
     }
 }
